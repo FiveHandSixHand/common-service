198b213540dbfb9d74eea23169da913b5f57f67e
Yoonho Cha <0119hoya@naver.com>
2026-03-30 14:35:51 +0900

    feat: 비지니스 에러 처리 핸들러 코드 작성

--- src/main/java/com/fhsh/daitda/exception/GlobalExceptionHandler.java
+++ src/main/java/com/fhsh/daitda/exception/GlobalExceptionHandler.java
@@ -1,9 +1,21 @@
 package com.fhsh.daitda.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        log.error("{} is occurred. message={}", errorCode.name(), e.getMessage(), e);
+
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.of(errorCode.name(), errorCode.getDescription()));
+    }
 }
