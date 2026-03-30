0270c1ec4df0596f61ab31eec99f3b760c0c8906
LAPTOP-NQ67GLI5\김다은 <tmzl235@naver.com>
2026-03-30 14:18:46 +0900

    Feat: 삭제자 길이 검증 로직 추가 및 필드 길이 제한(45자)을 상수로 설정

--- src/main/java/com/fhsh/daitda/domain/BaseUserEntity.java
+++ src/main/java/com/fhsh/daitda/domain/BaseUserEntity.java
@@ -15,15 +15,18 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseUserEntity extends BaseEntity {
+
+    private static final int AUDITOR_MAX_LENGTH = 45;
+
     @CreatedBy
-    @Column(length=45, updatable = false)
+    @Column(length = AUDITOR_MAX_LENGTH, updatable = false)
     protected String createdBy;
 
     @LastModifiedBy
-    @Column(length=45)
+    @Column(length = AUDITOR_MAX_LENGTH)
     protected String updatedBy;
 
-    @Column(length=45)
+    @Column(length = AUDITOR_MAX_LENGTH)
     protected String deletedBy;
 
     // Soft Delete 구현
@@ -31,6 +34,10 @@ public abstract class BaseUserEntity extends BaseEntity {
         // 중복 삭제로 인해 삭제 관련 필드가 업데이트되는 상황을 방지
         if (this.deletedAt != null) {
             return;
+        }
+
+        if (deletedBy != null && deletedBy.length() > AUDITOR_MAX_LENGTH) {
+            throw new IllegalArgumentException("삭제자 정보는 %d자를 초과할 수 없습니다.".formatted(AUDITOR_MAX_LENGTH));
         }
 
         // 삭제자가 없으면 SYSTEM 삭제로 간주
