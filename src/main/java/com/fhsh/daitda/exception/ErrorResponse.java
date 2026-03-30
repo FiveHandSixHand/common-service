4892f88d30c180eb0b3ddf131e57fa9b31e3bf95
Yoonho Cha <0119hoya@naver.com>
2026-03-30 14:37:06 +0900

    feat: 클라이언트용 에러응답 response작성

---
+++ src/main/java/com/fhsh/daitda/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.fhsh.daitda.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+    private final String error;
+    private final Object details;
+
+    public static ErrorResponse of(String error) {
+        return new ErrorResponse(error, null);
+    }
+
+    public static ErrorResponse of(String error, Object details) {
+        return new ErrorResponse(error, details);
+    }
+}
